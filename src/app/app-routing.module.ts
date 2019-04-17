56a94e7ac696adf6f8c47947a7bf35655083014d
matthew-trump <matthew.trump@gmail.com>
2019-04-17 15:54:15 -0700

    two app routes defined

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component'
+import { MessageComponent } from './message/message.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', component: WelcomeComponent },
+  { path: 'message', pathMatch: 'full', component: MessageComponent },
+  { path: 'welcome', pathMatch: 'full', component: WelcomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
