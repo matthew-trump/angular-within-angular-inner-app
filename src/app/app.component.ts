e418f6774ed09340b93bf1b61a400f3f7b691edf
matthew-trump <matthew.trump@gmail.com>
2019-04-17 15:54:03 -0700

    subscription to window event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { WindowCommunicatorService } from "./window-communicator.service"
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-within-angular-inner-app';
+  constructor(
+    private router: Router,
+    private windowCommuicatorService: WindowCommunicatorService) {
+    this.windowCommuicatorService.state$
+      .subscribe(state => {
+        if (state) {
+          if (typeof state.view !== 'undefined') {
+            this.router.navigate([state.view])
+          }
+        }
+      })
+  }
 }
